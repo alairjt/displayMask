c3f520516f6403ff4ee3bca7458caea02e2825b0
alair.tavares <alair.tavares@nexxera.com>
2016-06-30 11:08:03 -0300

    fix undefined input

--- display-mask.js
+++ display-mask.js
@@ -12,6 +12,7 @@
         
         function MaskFilter(input, mask) {
             var maskService = MaskService.create();
+            input = input || '';
             
             if (!angular.isObject(mask)) {
                 mask = { mask: mask };
